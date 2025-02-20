fa5b6da3a8a25ffc248e54caac62a08d165f5837
JDolven <joachim@jdolven.net>
2025-02-20 14:32:15 +0100

    Update __init__.py

--- components/mill/__init__.py
+++ components/mill/__init__.py
@@ -1,1 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import climate, uart
 
+DEPENDENCIES = ["uart"]
+
+mill_ns = cg.esphome_ns.namespace("mill")
+MillClimate = mill_ns.class_("MillClimate", climate.Climate, cg.Component)
+
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(MillClimate),
+    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
+    uart_component = await cg.get_variable(config["uart_id"])
+    cg.add(var.set_uart(uart_component))
