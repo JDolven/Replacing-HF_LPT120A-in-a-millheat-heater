21777153bc33887c86f8a0622e28f5a31114eff8
JDolven <joachim@jdolven.net>
2025-02-21 19:08:32 +0100

    Update climate.py

--- components/mill/climate.py
+++ components/mill/climate.py
@@ -2,22 +2,32 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, uart, network
 
-DEPENDENCIES = ["uart"]
+DEPENDENCIES = ["uart", "climate", "network"]
+
+
+from esphome.components.climate import (
+    ClimateMode,
+    CONF_CURRENT_TEMPERATURE,
+)
 
 mill_ns = cg.esphome_ns.namespace("mill")
-MillClimate = mill_ns.class_("MillClimate", climate.Climate, cg.Component)
+MillClimate = mill_ns.class_("MillClimate", uart.UARTDevice, climate.Climate, cg.Component)
 
 CONF_ID = "id"
 CONF_uart_id = "uart_id"
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(MillClimate),
-    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
-})
+CONFIG_SCHEMA = cv.All(
+    climate.CLIMATE_SCHEMA.extend(
+        {
+        cv.GenerateID(): cv.declare_id(MillClimate),
+        }
+    )
+    .extend(uart.UART_DEVICE_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
     await climate.register_climate(var, config)
-    uart_component = await cg.get_variable(config["uart_id"])
-    cg.add(var.set_uart(uart_component))
